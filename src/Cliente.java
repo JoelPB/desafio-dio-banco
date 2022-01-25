aa7272165ad9738de1c94551d45b5ff85d5facb2
Joel de Oliveira <coffee.program.joel@gmail.com>
2022-01-25 15:47:18 -0300

    Até Desenvolvendo saque, depósito, transferência e extrato

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,9 @@
 public class Cliente {
 
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
     private String nome;
 
     public String getNome() {
--- src/Conta.java
+++ src/Conta.java
@@ -1,4 +1,4 @@
-public class Conta implements IConta{
+public abstract class Conta implements IConta{
 
     private static final int AGENCIA_PADRAO = 1;
     private static int SEQUENCIAL = 1;
@@ -6,27 +6,30 @@ public class Conta implements IConta{
     protected int agencia;
     protected int numero;
     protected double saldo;
+    protected Cliente cliente;
 
 
-    public Conta() {
+    public Conta(Cliente cliente) {
         this.agencia = Conta.AGENCIA_PADRAO;
         this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
     }
 
 
     @Override
     public void sacar(double valor) {
-
+        this.saldo = saldo - valor;
     }
 
     @Override
     public void depositar(double valor) {
-
+        this.saldo = saldo + valor;
     }
 
     @Override
     public void transferir(double valor, Conta contaDestino) {
-
+        this.sacar(valor);
+        contaDestino.depositar(valor);
     }
 
     public int getAgencia() {
@@ -41,5 +44,11 @@ public class Conta implements IConta{
         return saldo;
     }
 
+    protected void imprimirInfosComuns(){
+        System.out.println(String.format("Agência: %d", this.agencia));
+        System.out.println(String.format("Número: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+        System.out.println(String.format("Cliente: %s", this.cliente.getNome()));
+    }
 
 }
